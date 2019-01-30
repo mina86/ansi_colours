feb19257780e1d63f1a6a1cb8d8bb4dfe918ae8a
Michal Nazarewicz <mina86@mina86.com>
2019-01-30 23:14:29 +0000

    Include γ=2.2 approximation in the benchmarks

--- tools/src/bin/luminance.rs
+++ tools/src/bin/luminance.rs
@@ -26,7 +26,7 @@ fn luminance_mean2g(r: u8, g: u8, b: u8) -> u8 {
     ((r as u16 + 2 * g as u16 + b as u16) / 4) as u8
 }
 
-fn luminance_weight(r: u8, g: u8, b: u8) -> u8 {
+fn luminance_weighted(r: u8, g: u8, b: u8) -> u8 {
     let y = 3568058 * r as u32 + 11998262 * g as u32 + 1210896 * b as u32;
     (y >> 24) as u8
     // FYI, the above gives the same results as:
@@ -63,29 +63,51 @@ fn luminance_isqrt(r: u8, g: u8, b: u8) -> u8 {
             b as u32 * b as u32 *  4730) >> 16) as u16)
 }
 
-fn luminance_xyz(r: u8, g: u8, b: u8) -> u8 {
+fn luminance_gamma22(r: u8, g: u8, b: u8) -> u8 {
+    fn to_linear(v: u8) -> f32 {
+        (v as f32 / 255.0).powf(2.2)
+    }
+
+    fn from_linear(v: f32) -> u8 {
+        ((v.powf(1.0 / 2.2) * 255.0) + 0.5) as u8
+    }
+
     from_linear(0.21267285140562248 * to_linear(r) +
                 0.715152155287818   * to_linear(g) +
                 0.07217499330655958 * to_linear(b))
 }
 
-fn to_linear(v: u8) -> f32 {
-    if v <= 10 {
-        v as f32 * (1.0 / 3294.6)
-    } else {
-        ((v as f32 + 14.025) * (1.0 / 269.025)).powf(2.4)
+fn luminance_xyz(r: u8, g: u8, b: u8) -> u8 {
+    fn to_linear(v: u8) -> f32 {
+        if v <= 10 {
+            v as f32 * (1.0 / 3294.6)
+        } else {
+            ((v as f32 + 14.025) * (1.0 / 269.025)).powf(2.4)
+        }
     }
-}
 
-fn from_linear(v: f32) -> u8 {
-    (if v <= 0.0031308 {
-        3294.6 * v
-    } else {
-        v.powf(1.0 / 2.4) * 269.025 - 14.025
-    } + 0.5) as u8
+    fn from_linear(v: f32) -> u8 {
+        (if v <= 0.0031308 {
+            3294.6 * v
+        } else {
+            v.powf(1.0 / 2.4) * 269.025 - 14.025
+        } + 0.5) as u8
+    }
+
+    from_linear(0.21267285140562248 * to_linear(r) +
+                0.715152155287818   * to_linear(g) +
+                0.07217499330655958 * to_linear(b))
 }
 
 fn distance(r: u8, g: u8, b: u8, grey: u8) -> f32 {
+    fn to_linear(v: u8) -> f32 {
+        if v <= 10 {
+            v as f32 * (1.0 / 3294.6)
+        } else {
+            ((v as f32 + 14.025) * (1.0 / 269.025)).powf(2.4)
+        }
+    }
+
     fn lab_from_y(y: f32) -> lab::Lab {
         // http://www.brucelindbloom.com/index.html?Eqn_XYZ_to_Lab.html
         let l = if y > 216.0 / 24389.0 {
@@ -125,7 +147,7 @@ fn try_function(name: &str, f: &Fn(u8, u8, u8)->u8) {
         n += 1;
     }
 
-    print!("{} {:5} ms;  distance: avg={:12.9} max={:12.9};  [{:x}]",
+    print!("{} {:5} ms;  distance: avg={:9.6} max={:9.6};  [{:x}]",
            name, elapsed, total_dist / n as f64, max_dist, hash);
 
     let black = f(0, 0, 0);
@@ -140,10 +162,11 @@ fn try_function(name: &str, f: &Fn(u8, u8, u8)->u8) {
 }
 
 fn main() {
-    try_function("mean  ", &luminance_mean);
-    try_function("mean2g", &luminance_mean2g);
-    try_function("weight", &luminance_weight);
-    try_function("square", &luminance_square);
-    try_function("isqrt ", &luminance_isqrt );
-    try_function("xyz   ", &luminance_xyz);
+    try_function("(r +  g + b) / 3", &luminance_mean);
+    try_function("(r + 2g + b) / 4", &luminance_mean2g);
+    try_function("weighted average", &luminance_weighted);
+    try_function("γ=2.0           ", &luminance_square);
+    try_function("γ=2 (no float)  ", &luminance_isqrt );
+    try_function("γ=2.2           ", &luminance_gamma22);
+    try_function("precise         ", &luminance_xyz);
 }
